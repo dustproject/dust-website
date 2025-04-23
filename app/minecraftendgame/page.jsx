96490b617f1357172c6ea2fe1cd9fe8955db9942
Dhrumil Shah <dhrumil.ketul@gmail.com>
2025-04-22 23:01:26 -0600

    smalll

--- app/minecraftendgame/page.jsx
+++ app/minecraftendgame/page.jsx
@@ -21,16 +21,17 @@ const MinecraftEndgamePage = () => {
         <div className="content-wrapper" style={{backgroundColor: "rgba(0, 0, 0, 0.5)", padding: "0px"}}>
           <div className="container">
             <div className="endgame-body">
-             Billions of Builds, Millions of Players, Thousands of Mods Have All Led to This… Minecraft Endgame. Where the Training Wheels Come Off.
+             Billions of Builds, Millions of Players, Thousands of Mods Have All Led to This… {" "}
+             <span style={{fontFamily: "Rubik Pixels"}}>MINECRAFT ENDGAME</span>. Where the Training Wheels Come Off.
             </div>
             <div className="endgame-body">
             Unlimited Players Will Live on One Shared Forever Map That Nobody Owns, Controls, or Can Shut Down. The Physics Will Be Impossible To Break and Players Will Build Real Societies and Economies Inside That Outlive Them.
             </div>
             <div className="endgame-body">
-            Looking To Collaborate With a Few Select Creators During Development.
+            Looking To Closely Collaborate With a Few Select Creators During Development that Get First Access & Support on Launch.
             </div>
             <Link href="https://discord.gg/J75hkmtmM4" className="join-button">
-              JOIN DISCORD
+              JOIN CREATOR DISCORD
             </Link>
 
 
@@ -45,7 +46,7 @@ const MinecraftEndgamePage = () => {
                 <Link href="https://ill.inc/" style={{color: "cyan"}}>
                     Senior Facebook, Google Engineers
                 </Link>. Public Protocol Technology Used By {" "}
-                <Link href="https://lattice.xyz/" style={{color: "cyan"}}>CCP Games</Link>.
+                <Link href="https://lattice.xyz/" style={{color: "cyan"}}>CCP Games (EVE Online)</Link>.
                 Mathematical Guarantee Advancements By {" "}
                 <Link href="https://0xparc.org/blog/programmable-cryptography-1" style={{color: "cyan"}}>MIT Researchers</Link>.
             </div>
