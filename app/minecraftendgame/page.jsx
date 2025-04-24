89b717412e81b0bc374dfb113a047a5532748345
Dhrumil Shah <dhrumil.ketul@gmail.com>
2025-04-23 22:19:15 -0600

    project

--- app/minecraftendgame/page.jsx
+++ app/minecraftendgame/page.jsx
@@ -58,32 +58,32 @@ const MinecraftEndgamePage = () => {
           <div className="container">
             <div className="endgame-body">
              Billions of Builds, Millions of Players, and Thousands of Mods Have All Led to This… {" "}
-             <span style={{fontFamily: "Rubik Pixels"}}>THE ENDGAME FOR MINECRAFTERS</span>.
+             <span style={{fontFamily: "Rubik Pixels"}}>A GRASSROOTS MINECRAFT FAN MOVEMENT BUILDING THE ENDGAME</span>.
             </div>
             <div className="endgame-body">
-            Unlimited Players Will Live on One Shared Forever Map That Nobody Can Own, Control, or Shutdown. Unbreakable Physics Will Present Serious Consequences and Players Will Build Real Societies That Outlive Them.
+            One Shared Forever Map With Unlimited Players That Nobody Can Own, Control, or Shutdown. Unbreakable Physics Present Serious Consequences and Players Build Real Societies That Outlive Them.
             </div>
             <div className="endgame-body">
-            Closely Collaborating With a Few Founding Creators During Development that Get First Access & Support on Launch.
+            Collaborate as a Founding Creator for First Access & Support on Launch.
             </div>
             <Link href="https://discord.gg/J75hkmtmM4" className="join-button">
               JOIN CREATOR DISCORD
             </Link>
-            <div className="endgame-subbody footer">
+            <div className="endgame-subbody footer" style={{paddingTop: "16px"}}>
                 This Is Impossible for Any Studio or Server To Do. So We’re Building {" "}
                 <Link href="https://dustproject.org/" style={{color: "cyan"}}>
                     Dust, a Public Protocol Like the Internet, but for the World’s Physics
                 </Link>. Everything That Happens Inside the World Is Mathematically Guaranteed To Follow Its Physics and Permanently Recorded for Anyone To Verify.	
             </div>
             <div className='endgame-subbody'>
-                Free Browser Client Developed By {" "}
+                Browser Client Tools Developed By {" "}
                 <Link href="https://ill.inc/" style={{color: "cyan"}}>
                     Senior Facebook, Google Engineers
                 </Link>. Public Protocol Technology Adopted By {" "}
                 <Link href="https://lattice.xyz/" style={{color: "cyan"}}>CCP Games (EVE Online)</Link>.
                 Mathematical Guarantee Advancements By {" "}
                 <Link href="https://0xparc.org/blog/programmable-cryptography-1" style={{color: "cyan"}}>MIT Researchers</Link>.
-                {/* Not affiliated with Mojang or Microsoft. */}
+                Not affiliated with or endorsed by Mojang/Microsoft.
             </div>
 
           </div>
