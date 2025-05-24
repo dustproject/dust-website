1ecc5868b8b1e2fad01dda38b27526fc68c60841
Dhrumil Shah <dhrumil.ketul@gmail.com>
2025-05-24 00:20:36 -0600

    stuff

--- app/page.jsx
+++ app/page.jsx
@@ -199,27 +199,26 @@ const LandingGrid = () => {
   );
 };
 
-
 const LandingGridTwo = () => {   
   return (    
     <div className="home-container">
       <TopographicMapBackground />
-      <div style={{ position: 'relative', zIndex: 1, paddingTop: "8rem", paddingBottom: "8rem" }}>
+      <div style={{ position: 'relative', zIndex: 1, paddingTop: "8rem", paddingBottom: "8rem", lineHeight: '0' }}>
         <div className='homeHeading'>GET A HEAD START</div>
         <div className="action-card-grid" >
           <div className="action-card">
-            <img src="https://files.facepunch.com/Alistair/128/04/2025/7j63/jungleUpdate_biome_16%20%281%29.jpg" className="card-image"/>
+            <img src="https://staticg.sportskeeda.com/editor/2022/03/422aa-16480454294357-1920.jpg" className="card-image"/>
             <div className='columnSubheading'>TRY TO SURVIVE</div>
             <div className='columnParagraph'>The world drains your energy. Eat or sleep—or die.</div>
           </div>
           <div className="action-card">
-            <img src="https://ccdn.g-portal.com/Gallery_MC_lushcave_0009_0d34f8ce09.png" className="card-image"/>
-            <div className='columnSubheading'>MINE & FARM RESOURCES</div>
+            <img src="https://www.pcgamesn.com/wp-content/sites/pcgamesn/2022/08/minecraft-diamond-119.jpg" className="card-image"/>
+            <div className='columnSubheading'>MINE & FARM DIGITAL MATTER</div>
             <div className='columnParagraph'>Extract, craft, harvest. Every block counts. Matter is scarce.</div>
           </div>
           <div className="action-card">
-            <img src="https://www.minecraft.net/content/dam/minecraftnet/archive/5b57dff89d5450b6cfcedb65ca2992be-VideoHeader.jpg" className="card-image"/>
-            <div className='columnSubheading'>PROTECT & PROGRAM TERRITORY</div>
+            <img src="https://preview.redd.it/x7uyp19l3ni51.jpg?width=1080&crop=smart&auto=webp&s=7c01112cccb8c1c7a0791f81cd055b06a0ea9bbd" className="card-image"/>
+            <div className='columnSubheading'>EXPAND & PROGRAM TERRITORY</div>
             <div className='columnParagraph'>Claim land, set its rules, and make it run code. Your domain, your laws.</div>
           </div>
         </div>
@@ -298,20 +297,120 @@ export default function Page() {
       </div>
     </div>
     <LandingGridTwo/>
-    <div style={{padding: "2rem", background: "#0a0a0a", borderTop: "1px dashed hsla(60, 68%, 94%, 0.1)", borderBottom: "1px dashed hsla(60, 68%, 94%, 0.1)"}}>
-      <div style={{position: 'relative'}}>
+    <div style={{background: "#0a0a0a", borderTop: "1px dashed hsla(60, 68%, 94%, 0.1)", borderBottom: "1px dashed hsla(60, 68%, 94%, 0.1)"}}>
+      <div style={{position: 'relative', lineHeight: '0'}}>
         <img src="/dusttexture.svg" className="full-image" />
         <Link href="/coalition" className="coalition-text" style={{position: 'absolute', top: '25%', left: '10%', transform: 'translate(-10%, -25%)', color: "white"}} ref={joinCoalitionRef}>
           FROM COSMIC TO CYBER DUST
-          </Link>
+        </Link>
       </div>
     </div>
     <div className="dust-protocol">
       <section className="dust-row">
         <div className='columnSubheading dust-headline'>
-          Virtual Worlds Will Matter More With Each Passing Year. We’ve Been Trying to Live in Them for Decades - But They Could Not Grow Beyond Theme Parks.
+          We’ve Been Trying to Live in Virtual Worlds for Decades. But They Can't Grow Beyond Theme Parks.
         </div>
       </section>
+      <section className="dust-grid theme-vs-world">
+        <div className="grid-label"></div>
+        <div className="grid-label">We Have Theme Parks</div>
+        <div className="grid-label">We Need Worlds</div>
+        <div className="grid-image">
+          <img src="/lastonline.svg" className='grid-image'/>
+        </div>
+
+        <div className="grid-cell">
+          <div className="grid-title">Temporary Admission</div>
+          <div className="grid-body">Owner eventually shuts down server. All creations & achievements are inevitably lost.</div>
+        </div>
+        <div className="grid-cell">
+          <div className="grid-title">Permanent Existence</div>
+          <div className="grid-body">Nobody can shut it down. Creations & achievements outlive players.</div>
+        </div>
+        <div className="grid-image">
+          <img src="/capsize.svg" className="grid-image" />
+        </div>
+        <div className="grid-cell">
+          <div className="grid-title">PLAYER LIMITS</div>
+          <div className="grid-body">Has a max player cap based on how much its owner can afford to scale.</div>
+        </div>
+        <div className="grid-cell">
+          <div className="grid-title">INFINITE CAPACITY</div>
+          <div className="grid-body">One continuous space that infinitely grows as players do.</div>
+        </div>
+        <div className="grid-image">
+          <img src="/malleable.svg" className="grid-image" />
+        </div>
+        <div className="grid-cell">
+          <div className="grid-title">MALLEABLE LOGIC</div>
+          <div className="grid-body">Owner can reset the world or arbitrarily rewrite its rules anytime.</div>
+        </div>
+        <div className="grid-cell">
+          <div className="grid-title">IMMUTABLE LAWS</div>
+          <div className="grid-body">Nobody can reset or force changes to the world’s rules.</div>
+        </div>
+        <div className="grid-image">
+          <img src="/monopoly.svg" className="grid-image" />
+        </div>
+        <div className="grid-cell">
+          <div className="grid-title">MONOPOLY RESOURCES</div>
+          <div className="grid-body">Owner can infinitely spawn, arbitrarily wipe, or nerf resources anytime.</div>
+        </div>
+        <div className="grid-cell">
+          <div className="grid-title">REAL RESOURCES</div>
+          <div className="grid-body">Nobody can spawn, delete, or nerf resources. Supply and behavior are provably fixed.</div>
+        </div>
+        <div className="grid-image">
+          <img src="/illegal.svg" className="grid-image" />
+        </div>
+        <div className="grid-cell">
+          <div className="grid-title">Gatekept Agency</div>
+          <div className="grid-body">
+            Studios build trusted first-party features. Players can't due to gatekeeping or regulation.
+          </div>
+        </div>
+        <div className="grid-cell">
+          <div className="grid-title">CREATIVE FREEDOM</div>
+          <div className="grid-body">
+            Player creations are trusted, first-party, and integrate with the whole internet & economy. Redstone on steroids.
+          </div>
+        </div>
+      </section>
+      <section className="dust-grid-sub theme-vs-world-sub">
+        <div className="grid-image">
+            <img src="/serverowner.png" className='grid-image'/>
+        </div>
+        <div className='grid-cell'>
+          <div className="grid-title">Theme Parks Are All We Get With What We Have - Servers</div>
+          <div className="grid-body">
+          Only the owner really knows what’s happening inside. They can change anything at anytime, then unplug it when they leave or run out of money.
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
       <section className="dust-row">
         <div className='columnSubheading dust-headline'>
@@ -321,7 +420,10 @@ export default function Page() {
 
       <section className="dust-row">
         <div className='columnSubheading dust-headline'>
-          As Players Program Territories, Societies Emerge and Break Into the Internet and Economy, Organizations Contribute Compute and Clients, Resources Accrue Value and Spark Rushes and Wars... How Far Does Dust Go?
+          Virtual Worlds Will Matter More With Each Passing Year.
+          <br/>
+          <br/>
+          As Players Program Territories, Emergent Societies Break Into the Internet and Economy, Organizations Contribute Compute and Clients, Resources Accrue Value and Spark Rushes and Wars... How Far Will Dust Go?
         </div>
       </section>
 
@@ -330,23 +432,10 @@ export default function Page() {
 
 
 
-      {/* <section className="dust-grid theme-vs-world">
-        <div className="grid-label">Image</div>
-        <div className="grid-label">Theme Park</div>
-        <div className="grid-label">World</div>
 
-        <div className="grid-image">[Image1]</div>
-        <div className="grid-text">Admins can change anything</div>
-        <div className="grid-text">Laws are immutable</div>
 
-        <div className="grid-image">[Image2]</div>
-        <div className="grid-text">Resources are spawned</div>
-        <div className="grid-text">Resources are scarce and fixed</div>
 
-        <div className="grid-image">[Image3]</div>
-        <div className="grid-text">Servers shut down</div>
-        <div className="grid-text">World runs peer-to-peer, forever</div>
-      </section>
+
       <section className="dust-grid timeline">
         <div className="timeline-block">Bitcoin<br/><span>Scarcity via Proof of Work</span></div>
         <div className="timeline-block">Ethereum<br/><span>Smart Contracts</span></div>
@@ -356,7 +445,7 @@ export default function Page() {
       <section className="dust-vision">
         <h4>One Map. One History. No Resets.</h4>
         <p>[Insert: Dome diagram, territory logic, PvP laws, peer-to-peer infra, etc.]</p>
-      </section> */}
+      </section>
 
     </div>
   </div>
