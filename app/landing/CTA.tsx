82ab8df90261095622c974091efc73060277ee11
karooolis <hello@karolisram.com>
2025-06-12 14:33:49 +0300

    add access url

--- app/landing/CTA.tsx
+++ app/landing/CTA.tsx
@@ -4,7 +4,7 @@ export function CTA() {
   return (
     <div className="mt-[60px] flex flex-col sm:flex-row gap-[40px]">
       <LinkButton
-        href="#"
+        href="/access"
         title="Get access"
         description="Join Discord for early access to the Dust world."
       />
