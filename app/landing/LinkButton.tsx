8f7c9abec74ce47bfe3071ae34b4dc650b7be380
karooolis <hello@karolisram.com>
2025-06-12 13:34:08 +0300

    add group hover underline

--- app/landing/LinkButton.tsx
+++ app/landing/LinkButton.tsx
@@ -12,9 +12,11 @@ export function LinkButton({
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className="w-full p-[20px] border border-[#929292] border-dashed inline-flex flex-col gap-2 sm:w-[200px]"
+      className="w-full p-[20px] border border-[#929292] border-dashed inline-flex flex-col gap-2 sm:w-[200px] group"
     >
-      <span className="text-[16px] uppercase">{title}</span>
+      <span className="text-[16px] uppercase group-hover:underline">
+        {title}
+      </span>
       <span className="text-[16px] text-white/50 uppercase">{description}</span>
     </a>
   );
