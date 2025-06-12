b39c8b1b32eedfd09605cd1e877173b6d3a102c9
karooolis <hello@karolisram.com>
2025-06-12 18:39:01 +0300

    border quickfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,21 +12,4 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    function({ addUtilities }) {
-      const newUtilities = {
-        '.dashed-border': {
-          'background-image': "url(\"data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23929292FF' stroke-width='1' stroke-dasharray='6%2c 7' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e\")",
-          'transition': 'background-image 0.3s ease',
-        },
-        '.dashed-border:hover': {
-          'background-image': "url(\"data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23FFFFFF' stroke-width='1' stroke-dasharray='6%2c 7' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e\")",
-        },
-        '.dashed-border:focus': {
-          'background-image': "url(\"data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23FFFFFF' stroke-width='1' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e\")",
-        },
-      }
-      addUtilities(newUtilities)
-    }
-  ],
 };
