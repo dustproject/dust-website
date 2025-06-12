41141bd97b92db7fe9f3b239c80cb80275a3ffe4
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-06-12 13:33:17 -0600

    fix network explorer card link

--- app/(docs)/what/page.jsx
+++ app/(docs)/what/page.jsx
@@ -42,11 +42,11 @@ const LearnPage = () => {
       <div className="learn-description-left">
         <div className='learn-title'>Code civilization.</div>
         <div className='learn-body'>
-        On top of the world's fixed rules of physics, you code civilization—reputation, currency, governments, markets, and more. 
+        On top of the world's fixed rules of physics, you code civilization—reputation, currency, governments, markets, and more.
         </div>
         <div className='learn-body'>
         Your code runs live, shapes everyone's gameplay, and persists even after you leave.
-        It becomes a native part of the world itself, equal to the fixed rules of physics—not an isolated mod.        
+        It becomes a native part of the world itself, equal to the fixed rules of physics—not an isolated mod.
         </div>
         <div className='learn-body'>
         Your code commands machines directly—turning chests into shops and doors into toll gates. Build on top of other players’ live code, and they build on top of yours. Connect your code to anything online—Reddit, Discord, Dollars.
@@ -85,7 +85,7 @@ const LearnPage = () => {
       </div>
       <div className="learn-description-right">
         <div className="learn-right-grid">
-          <Link href="https://explorer.mud.dev/garnet/worlds/0x4C619a1AA1b7C30137b2e8f4A78327C68D33A8ac/explore?tableId=0x74627063746f6b656e0000000000000042616c616e6365730000000000000000&query=SELECT%2520%2522account%2522%252C%2520%2522value%2522%2520FROM%2520%2522pctoken__Balances%2522%2520LIMIT%252010%2520OFFSET%25200%253B&page=0&pageSize=10&readonly=true" className="learn-card">
+          <Link href="https://explorer.mud.dev/redstone/worlds/0x253eb85B3C953bFE3827CC14a151262482E7189C" className="learn-card">
             <img src="/explorer.svg" className="learn-card-image" />
             <div className="learn-card-overlay">
             <div className="learn-card-label">Network Explorer</div>
