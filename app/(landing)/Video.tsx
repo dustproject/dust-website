3438f9cde289f6f7e5f5a771258837c70092cef8
karooolis <hello@karolisram.com>
2025-06-13 16:51:44 +0300

    do not play video on mobile

--- app/(landing)/Video.tsx
+++ app/(landing)/Video.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { Stream, StreamPlayerApi } from "@cloudflare/stream-react";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const VIDEO_ID = "da4a72e9380f416c68c195daeec0e08c";
 
 export function Video() {
   const videoRef = useRef<StreamPlayerApi>(null);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(/iPhone|iPad|iPod|Android/i.test(navigator.userAgent));
+  }, []);
 
   useEffect(() => {
     const handleFullscreenChange = () => {
@@ -37,26 +42,28 @@ export function Video() {
 
   return (
     <div className="relative group overflow-hidden aspect-[16/9]">
-      <div
-        className="absolute inset-0 z-10 transition-all duration-300 cursor-pointer group-hover:bg-black/50"
-        onClick={handlePlayClick}
-      />
-      <button
-        type="button"
-        onClick={handlePlayClick}
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 text-white transition-opacity opacity-0 group-hover:opacity-100 duration-300 text-center flex flex-col gap-0 cursor-pointer"
-      >
-        <span className="text-[16px] leading-none">Watch in full screen</span>
-        <span className="text-[14px] text-white/70">with audio</span>
-      </button>
+      <div className={isMobile ? "hidden" : "block"}>
+        <div
+          className="absolute inset-0 z-10 transition-all duration-300 cursor-pointer group-hover:bg-black/50"
+          onClick={handlePlayClick}
+        />
+        <button
+          type="button"
+          onClick={handlePlayClick}
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 text-white transition-opacity opacity-0 group-hover:opacity-100 duration-300 text-center flex flex-col gap-0 cursor-pointer"
+        >
+          <span className="text-[16px] leading-none">Watch in full screen</span>
+          <span className="text-[14px] text-white/70">with audio</span>
+        </button>
+      </div>
 
       <div className="absolute -top-0 left-0 h-full w-full scale-140">
         <Stream
           src={VIDEO_ID}
           streamRef={videoRef}
-          autoplay
+          autoplay={!isMobile}
           controls
-          muted
+          muted={!isMobile}
           loop
           startTime={74}
         />
