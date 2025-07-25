9055db591f92193cbb5f9005a81d88ee6fe496c5
Dhrumil Shah <dhrumil.ketul@gmail.com>
2025-07-24 23:25:09 -0600

    new

--- app/10-years-ethereum/Features.tsx
+++ app/10-years-ethereum/Features.tsx
@@ -1,20 +1,18 @@
+import { cn } from "../../utils/cn";
+
 export function Features() {
   return (
-    <div className="space-y-[25px] xl:space-y-[35px] w-full xl:w-[430px]">
-      <Feature
-        description="In honor of Ethereum's 10 year anniversary..."
-      />
-      <Feature
-        description="We are building the first fully onchain monument together."
-      />
-      <Feature
+    <div className="space-y-[45px] xl:space-y-[55px] w-full xl:w-[440px]">
+      <FeatureSmall
+        title="Build Ethereum's 10 Year Monument."
         description="Gather and place blocks to complete the monument's blueprint."
+        descriptionTwo="Power the force field that will protect the monument forever."
+        descriptionThree="Every contribution is recorded onchain."
       />
-      <Feature
-        description="Supply energy to the protective force field that will preserve the monument."
-      />
-      <Feature
-        description="Every contribution is recorded onchain forever."
+      <FeatureSmall
+        title="Inside Ethereum Itself."
+        description="The monument is built inside DUST, a fully onchain world, using its provably scarce materials."
+        descriptionTwo="DUST is programmable, so players deployed the smart contracts that define this monument's blueprint and govern its force field."
       />
     </div>
   );
@@ -32,8 +30,38 @@ export function Feature({
       <h3 className="text-[20px] sm:text-[22px] font-bold tracking-wide">
         {title}
       </h3>
-      <p className="text-[20px] sm:text-[22px] italic font-extralight leading-normal">
+      <p className="text-[20px] sm:text-[22px] font-extralight leading-normal">
         {description}
+      </p>
+    </div>
+  );
+}
+
+
+export function FeatureSmall({
+  title,
+  description,
+  descriptionTwo,
+  descriptionThree
+}: {
+  title?: string;
+  description: React.ReactNode;
+  descriptionTwo: React.ReactNode;
+  descriptionThree?: React.ReactNode;
+}) {
+  return (
+    <div className="space-y-2">
+      <h3 className="text-[20px] sm:text-[22px] font-bold tracking-wide">
+        {title}
+      </h3>
+      <p className="text-[20px] sm:text-[22px] font-extralight leading-normal italic">
+        {description}
+      </p>
+      <p className="text-[20px] sm:text-[22px] font-extralight leading-normal italic">
+        {descriptionTwo}
+      </p>
+      <p className="text-[20px] sm:text-[22px] font-extralight leading-normal italic">
+        {descriptionThree}
       </p>
     </div>
   );
--- app/10-years-ethereum/Tweet.tsx
+++ app/10-years-ethereum/Tweet.tsx
@@ -2,17 +2,17 @@ export function Tweet() {
   return (
     <a
       // TODO: update with deeplink
-      href="https://dustproject.org"
+      href="https://x.com/dust_org"
       target="_blank"
       rel="noopener noreferrer"
-      className="flex flex-col md:flex-row gap-8 px-[16px] md:px-[24px] py-[16px] xl:flex-1 custom-dashed-border-dark"
+      className="flex flex-col md:flex-row gap-2 px-[16px] md:px-[24px] py-[16px] xl:flex-1 custom-dashed-border-dark"
     >
-      <div className="text-[16px] opacity-50 leading-normal flex-shrink-0">
-        HAPPENING IN DUST
+      <div className="text-[16px] opacity-50 leading-none flex-shrink-0">
+      JULY 30 2025
       </div>
-      <div className="text-[16px] leading-normal">
-      The monument is being built inside DUST, a fully onchain world, using <strong>provably scarce materials</strong> like Gold and Glass.
-      DUST is deeply programmable—<strong>players deployed custom smart contracts</strong> to define the monument's blueprints and record contributions.
+      <div className="text-[16px] leading-none uppercase">
+      CELEBRATE 10 YEARS OF ETHEREUM. BUILD THE FIRST FULLY ONCHAIN MONUMENT.
+      PARTICIPATE IN SMART CONTRACT DRIVEN CONSTRUCTION.
       </div>
     </a>
   );
